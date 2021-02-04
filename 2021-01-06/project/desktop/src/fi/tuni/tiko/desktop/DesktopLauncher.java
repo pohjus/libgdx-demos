1df6296ef947bd89a09b75e9611654114bdcabfb
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-02-04 08:35:07 +0200

    tiled maps demo

---
+++ 2021-01-06/project/desktop/src/fi/tuni/tiko/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package fi.tuni.tiko.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.tuni.tiko.TiledMapDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
+
+		new LwjglApplication(new TiledMapDemo(), config);
+	}
+}
